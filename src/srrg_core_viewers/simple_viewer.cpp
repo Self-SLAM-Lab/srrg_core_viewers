d8b6dfb3f2e5971ee03d6641b1a813e9f458563d
dom <schdomin@gmail.com>
2018-05-04 14:19:51 +0200

    exposed standard camera for modification of clipping planes

--- src/srrg_core_viewers/simple_viewer.cpp
+++ src/srrg_core_viewers/simple_viewer.cpp
@@ -1,95 +1,66 @@
 #include "simple_viewer.h"
 #include <cstring>
 
-// Macro helpers for identifying the version number of QGLViewer
-// QGLViewer changed some parts of its API in version 2.6.
-// The following preprocessor hack accounts for this.
-#if (((QGLVIEWER_VERSION & 0xff0000) >> 16) >= 2 && ((QGLVIEWER_VERSION & 0x00ff00) >> 8) >= 6)
-#define qglv_real qreal
-#else
-#define qglv_real float
-#endif
+
 
 namespace srrg_core_viewers {
-  
-  using namespace std;
-  
-  class StandardCamera: public qglviewer::Camera {
-  public:
-    StandardCamera(): _standard(true) {}
-  
-    qglv_real zNear() const {
-      if(_standard) { return qglv_real(0.001f); } 
-      else { return Camera::zNear(); } 
-    }
 
-    qglv_real zFar() const {  
-      if(_standard) { return qglv_real(10000.0f); } 
-      else { return Camera::zFar(); }
-    }
+SimpleViewer::SimpleViewer(QWidget* parent): QGLViewer(parent),
+                                             _standard_camera(0),
+                                             _last_key_event(QEvent::None, 0, Qt::NoModifier),
+                                             _last_key_event_processed(true) {}
 
-    bool standard() const { return _standard; }  
-    void setStandard(bool s) { _standard = s; }
+void SimpleViewer::init() {
+  // Init QGLViewer.
+  QGLViewer::init();
+  // Set background color light yellow.
+  // setBackgroundColor(QColor::fromRgb(255, 255, 194));
 
-  protected:
-    bool _standard;
-  };
 
-  SimpleViewer::SimpleViewer(QWidget* parent): QGLViewer(parent),
-                                               _last_key_event(QEvent::None, 0, Qt::NoModifier),
-                                               _last_key_event_processed(true) {}
+  // Set background color white.
+  glClearColor(1.0f,1.0f,1.0f,1.0f);
+  //setBackgroundColor(QColor::fromRgb(255, 255, 255));
 
-  void SimpleViewer::init() {
-    // Init QGLViewer.
-    QGLViewer::init();
-    // Set background color light yellow.
-    // setBackgroundColor(QColor::fromRgb(255, 255, 194));
+  // Set some default settings.
+  glEnable(GL_LINE_SMOOTH);
+  glEnable(GL_BLEND);
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_NORMALIZE);
+  glShadeModel(GL_FLAT);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    
-    // Set background color white.
-    glClearColor(1.0f,1.0f,1.0f,1.0f);
-    //setBackgroundColor(QColor::fromRgb(255, 255, 255));
+  // Don't save state.
+  setStateFileName(QString::null);
 
-    // Set some default settings.
-    glEnable(GL_LINE_SMOOTH);
-    glEnable(GL_BLEND); 
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_NORMALIZE);
-    glShadeModel(GL_FLAT);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  // Mouse bindings.
+  setMouseBinding(Qt::NoModifier, Qt::RightButton, CAMERA, ZOOM);
+  setMouseBinding(Qt::NoModifier, Qt::MidButton, CAMERA, TRANSLATE);
+  setMouseBinding(Qt::ControlModifier, Qt::LeftButton, RAP_FROM_PIXEL);
 
-    // Don't save state.
-    setStateFileName(QString::null);
+  // Replace camera.
+  qglviewer::Camera *oldcam = camera();
+  _standard_camera = new StandardCamera();
+  setCamera(_standard_camera);
+  _standard_camera->setPosition(qglviewer::Vec(0.0f, 0.0f, 10.0f));
+  _standard_camera->setUpVector(qglviewer::Vec(0.0f, 0.0f, 0.0f));
+  _standard_camera->lookAt(qglviewer::Vec(0.0f, 0.0f, 0.0f));
+  delete oldcam;
 
-    // Mouse bindings.
-    setMouseBinding(Qt::NoModifier, Qt::RightButton, CAMERA, ZOOM);
-    setMouseBinding(Qt::NoModifier, Qt::MidButton, CAMERA, TRANSLATE);
-    setMouseBinding(Qt::ControlModifier, Qt::LeftButton, RAP_FROM_PIXEL);
+}
 
-    // Replace camera.
-    qglviewer::Camera *oldcam = camera();
-    qglviewer::Camera *cam = new StandardCamera();
-    setCamera(cam);
-    cam->setPosition(qglviewer::Vec(0.0f, 0.0f, 10.0f));
-    cam->setUpVector(qglviewer::Vec(0.0f, 0.0f, 0.0f));
-    cam->lookAt(qglviewer::Vec(0.0f, 0.0f, 0.0f));
-    delete oldcam;
+void SimpleViewer::keyPressEvent(QKeyEvent *e) {
+  QGLViewer::keyPressEvent(e);
+    _last_key_event = *e;
+    _last_key_event_processed=false;
+}
 
-  }
+QKeyEvent* SimpleViewer::lastKeyEvent() {
+  if (_last_key_event_processed)
+    return 0;
+  return &_last_key_event;
+}
 
-  void SimpleViewer::keyPressEvent(QKeyEvent *e) {
-    QGLViewer::keyPressEvent(e);
-      _last_key_event = *e;
-      _last_key_event_processed=false;
-  }
-
-  QKeyEvent* SimpleViewer::lastKeyEvent() {
-    if (_last_key_event_processed)
-      return 0;
-    return &_last_key_event;
-  }
-
-  void SimpleViewer::keyEventProcessed() {
-    _last_key_event_processed = true;
-  }
+void SimpleViewer::keyEventProcessed() {
+  _last_key_event_processed = true;
+}
 }
--- src/srrg_core_viewers/simple_viewer.h
+++ src/srrg_core_viewers/simple_viewer.h
@@ -1,7 +1,19 @@
 #pragma once
-
 #include <QKeyEvent>
 #include <QGLViewer/qglviewer.h>
+
+
+
+// Macro helpers for identifying the version number of QGLViewer
+// QGLViewer changed some parts of its API in version 2.6.
+// The following preprocessor hack accounts for this.
+#if (((QGLVIEWER_VERSION & 0xff0000) >> 16) >= 2 && ((QGLVIEWER_VERSION & 0x00ff00) >> 8) >= 6)
+#define qglv_real qreal
+#else
+#define qglv_real float
+#endif
+
+
 
 namespace srrg_core_viewers {
 
@@ -26,29 +38,63 @@ namespace srrg_core_viewers {
       v.keyEventProcessed();<br>
     }<br>
  */
-  class SimpleViewer: public QGLViewer {
+class SimpleViewer: public QGLViewer {
+
+  //! @class internal camera class
+  class StandardCamera: public qglviewer::Camera {
   public:
+    StandardCamera(): _standard(true) {}
 
-    //! ctor
-    SimpleViewer(QWidget* parent = 0);
+    qglv_real zNear() const {
+      if(_standard) { return qglv_real(_z_near); }
+      else { return Camera::zNear(); }
+    }
 
-    //! init method, opens the gl viewport and sets the key bindings
-    void init();
+    qglv_real zFar() const {
+      if(_standard) { return qglv_real(_z_far); }
+      else { return Camera::zFar(); }
+    }
 
-    //! callback invoked by the application on new key event. It saves the last event in
-    //! a member variable
-    virtual void keyPressEvent(QKeyEvent *e);
-
-    //! returns the last key pressed since invoking keyEventProcessed();
-    QKeyEvent* lastKeyEvent();
-
-    //! call this to clear the events, after processing them
-    void keyEventProcessed();
+    bool standard() const { return _standard; }
+    void setStandard(bool s) { _standard = s; }
+    void setZNear(const float& z_near_) {_z_near = z_near_;}
+    void setZFar(const float& z_far_) {_z_far = z_far_;}
 
   protected:
-
-    QKeyEvent _last_key_event;
-    bool _last_key_event_processed;
-
+    bool _standard;
+    float _z_near = 0.001f;
+    float _z_far  = 10000.0f;
   };
+
+public:
+
+  //! ctor
+  SimpleViewer(QWidget* parent = 0);
+
+  //! init method, opens the gl viewport and sets the key bindings
+  void init();
+
+  //! callback invoked by the application on new key event. It saves the last event in
+  //! a member variable
+  virtual void keyPressEvent(QKeyEvent *e);
+
+  //! returns the last key pressed since invoking keyEventProcessed();
+  QKeyEvent* lastKeyEvent();
+
+  //! call this to clear the events, after processing them
+  void keyEventProcessed();
+
+  //! @brief modifiable camera
+  StandardCamera* standardCamera() {return _standard_camera;}
+
+protected:
+
+  //! @brief customized 'standard' camera instance
+  StandardCamera* _standard_camera;
+
+  //! key handling
+  QKeyEvent _last_key_event;
+  bool _last_key_event_processed;
+
+};
 }
